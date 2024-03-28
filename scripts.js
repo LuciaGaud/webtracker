18f7ec099c32fc6fcb7483661c47246cb333845e
MarleneGray <marlenegray666@gmail.com>
2024-03-28 15:17:12 +0000

    adding a script which automatically add and remove classes for a better visualization of the sorting feature

--- scripts.js
+++ scripts.js
@@ -1,47 +1,76 @@
 //-----Not server side Javascript---------
-function sortTableByColumn(columnName) {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("csvTable");
-    switching = true;
-    dir = "asc"; // Set the sorting direction to ascending:
+function sortTableByColumn(columnName, tableName) {
+    var table = document.getElementById(tableName);
+    var headers = table.getElementsByTagName("TH");
+    var rows = table.getElementsByTagName("TR");
+    var switching = true;
+    var shouldSwitch;
+    var dir = "asc";
+    var switchcount = 0;
+    var i, x, y;
+    var headerIndex = -1;
+
+    // Remove existing sorting classes from all headers
+    for (var j = 0; j < headers.length; j++) {
+        headers[j].classList.remove("th-sort-asc", "th-sort-desc");
+    }
+
+    // Find the index of the header cell that matches columnName
+    for (var i = 0; i < headers.length; i++) {
+        if (headers[i].getAttribute('data-column') === columnName) {
+            headerIndex = i;
+            headers[i].classList.add("th-sort-asc"); // Set the initial sort class
+            break;
+        }
+    }
+    
+    // If the header cell was not found, exit the function
+    if (headerIndex === -1) return;
 
     while (switching) {
-        switching = false;
-        rows = table.getElementsByTagName("TR");
-        for (i = 1; i < rows.length - 1; i++) {
-            shouldSwitch = false;
+        switching = false; // Start by saying no switching is needed
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false; // Start by saying there should be no switching
             x = rows[i].querySelector("[data-column='" + columnName + "']");
             y = rows[i + 1].querySelector("[data-column='" + columnName + "']");
-            if (dir == "asc") {
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir == "desc") {
-                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                    shouldSwitch = true;
-                    break;
-                }
+
+            // Determine if a switch should occur based on the sorting direction
+            if ((dir === "asc" && x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) ||
+                (dir === "desc" && x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase())) {
+                shouldSwitch = true; // Mark that a switch is needed and break out of the loop
+                break;
             }
         }
         if (shouldSwitch) {
+            // If a switch is needed, perform it and continue the while loop
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
-            switchcount++;
-        } else {
-            if (switchcount == 0 && dir == "asc") {
-                dir = "desc";
-                switching = true;
-            }
+            switchcount++; // Increment the switch count for this pass
+        }  else if (switchcount === 0 && dir === "asc") {
+            dir = "desc";
+            switching = true;
+        } else if (switchcount === 0 && dir === "desc") {
+            dir = "asc";
+            switching = true;    
         }
+        // At the end of each pass, update the header classes
+if (dir === "asc") {
+    th.classList.remove("th-sort-desc");
+    th.classList.add("th-sort-asc");
+} else {
+    th.classList.remove("th-sort-asc");
+    th.classList.add("th-sort-desc");
+}
+
     }
 }
 
+
 function downloadTableAsXLS() {
     // Select the table you want to download
-    var table = document.getElementById('csvTable');
+    var table = document.getElementById("csvTable");
     var html = table.outerHTML;
-  
+
     // Create a fake HTML document with the table embedded
     var htmlDocument = `
       <html>
@@ -53,18 +82,18 @@ function downloadTableAsXLS() {
         </body>
       </html>
     `;
-  
+
     // Download as .xls
-    var fileName = 'table.xls';
+    var fileName = "table.xls";
     var blob = new Blob([htmlDocument], {
-      type: 'application/vnd.ms-excel'
+        type: "application/vnd.ms-excel",
     });
-    
+
     var url = URL.createObjectURL(blob);
-    var a = document.createElement('a');
+    var a = document.createElement("a");
     a.href = url;
     a.download = fileName;
     a.click();
-    
+
     URL.revokeObjectURL(url);
-  }
+}
